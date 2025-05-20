4dc92e501818e50c4fb458c44d5592d06334d927
jonathan Diaz <jonatham01@hotmail.com>
2025-05-20 08:48:53 -0500

    getter and setter methods are added to hotel entities

--- app/src/main/java/hotel/manager/entities/HotelRequest.java
+++ app/src/main/java/hotel/manager/entities/HotelRequest.java
@@ -6,4 +6,44 @@ public class HotelRequest {
     private String city;
     private String state;
     private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- app/src/main/java/hotel/manager/entities/HotelResponse.java
+++ app/src/main/java/hotel/manager/entities/HotelResponse.java
@@ -10,4 +10,60 @@ public class HotelResponse {
     private String state;
     private String country;
     private List<HotelPhoneResponse> hotelPhones;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<HotelPhoneResponse> getHotelPhones() {
+        return hotelPhones;
+    }
+
+    public void setHotelPhones(List<HotelPhoneResponse> hotelPhones) {
+        this.hotelPhones = hotelPhones;
+    }
 }
