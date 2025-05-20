10940e0c1a8bec326a657b1de139ee0acb8fde5b
jonathan Diaz <jonatham01@hotmail.com>
2025-05-20 07:55:50 -0500

    HotelRetroInterface is updated and settled

--- app/src/main/java/hotel/manager/retrofitInterfaces/HotelRetroInterface.java
+++ app/src/main/java/hotel/manager/retrofitInterfaces/HotelRetroInterface.java
@@ -6,17 +6,44 @@ import hotel.manager.entities.HotelPhoneRequest;
 import hotel.manager.entities.HotelResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface HotelRetroInterface {
-    @GET("hotels")
-    Call<List<HotelResponse>> getAllHotelCall();
+    @Headers({"Content-Type: application/json"})
+    @GET("show/all")
+    Call<List<HotelResponse>> getAllHotelCall(@Header("Authorization") String authToken);
 
-    @GET("hotels/{id}")
-    Call<HotelResponse> getOneHotelCall();
 
-    @POST("hotels")
-    Call<HotelResponse> createOneHotel(@Body HotelPhoneRequest request);
+    @Headers({"Content-Type: application/json"})
+    @GET("find/attributes")
+    Call<HotelResponse> getOneHotelCall(
+            @Header("Authorization") String authToken,
+            @Header("Authorization") String country,
+            @Header("Authorization") String state,
+            @Header("Authorization") String city,
+            @Header("Authorization") String name
+    );
+
+    @Headers({"Content-Type: application/json"})
+    @POST("new")
+    Call<HotelResponse> createOneHotel(
+            @Header("Authorization") String authToken,
+            @Body HotelPhoneRequest request);
+
+    @Headers({"Content-Type: application/json"})
+    @PUT("update/{id}")
+    Call<HotelResponse> createOneHotel(
+            @Header("Authorization") String authToken,
+            @Path("id") Integer id, @Body HotelPhoneRequest request);
+
+    @Headers({"Content-Type: application/json"})
+    @DELETE("{id}")
+    Call<?> deleteHotel(@Header("Authorization") String authToken, @Path("id") Integer id);
+
 }
