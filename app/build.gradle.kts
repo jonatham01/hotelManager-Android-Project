8e290171025c052e9c080a9f403616c03f2fc5e4
jonathan Diaz <jonatham01@hotmail.com>
2025-05-20 07:35:45 -0500

    libreries are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
